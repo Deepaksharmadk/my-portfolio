cec668eea47fb4d84e936bf4a986f045aa04f85f
deepaksharmadk <dks847203@gmail.com>
2024-06-22 12:05:54 +0530

    create express instance to codebase

--- backend/src/index.js
+++ backend/src/index.js
@@ -8,14 +8,13 @@ dotenv.config();
 app.use(
   cors({
     origin: "http://localhost:3000",
+    credentials: true,
   })
 );
-
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 const PORT = process.env.PORT || 5000;
-mongoose
-  .connect(process.env.MONGO_URL)
-  .then(() => {
-    console.log("database connected");
-  })
-  .catch((err) => {});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
