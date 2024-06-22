ea6cd42f15c85196f9506874bd6d80d261e2cdb4
deepaksharmadk <dks847203@gmail.com>
2024-06-22 12:36:10 +0530

    connect db to codebase

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,7 +2,7 @@ import express from "express";
 import bodyParser from "body-parser";
 import cors from "cors";
 import dotenv from "dotenv";
-import mongoose from "mongoose";
+import dbconnection from "./dbconnection/db.js";
 const app = express();
 dotenv.config();
 app.use(
@@ -14,7 +14,10 @@ app.use(
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 const PORT = process.env.PORT || 5000;
-
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+dbconnection()
+  .then(
+    app.listen(PORT, () => {
+      console.log(`Server running on port ${PORT}`);
+    })
+  )
+  .catch((err) => console.log(`mongodb connection failed`, err));
